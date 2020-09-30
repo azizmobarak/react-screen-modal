8fe26dc9eb59ea527d080952f4798bb97c9daeb5
aziz mobarak <mobarakaziz9@gmail.com>
2020-09-30 15:41:27 +0100

    init with some existing work

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-import { ExampleComponent } from 'react-screen-modal'
-import 'react-screen-modal/dist/index.css'
+import { CoverModal } from 'react-screen-modal'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+
+const [show,showmodal]=useState(false)
+
+const openmodal=()=>{
+  showmodal(true);
+}
+const closemodal =()=>{
+  showmodal(false)
+}
+
+    return <div>
+
+  <div style={{ display:"flex",flexDirection:"column",justifyContent:"center" ,alignItems:"center",height:"100vh" }} >
+
+   <button style={{ width:200,color:"green" }} onClick={()=>openmodal()} >show</button>
+
+  </div>
+
+  <CoverModal
+    show={show}
+    onClose={closemodal}
+    color = "aqua"
+    closeButtonColor="#fff"
+    closeButtonRight={true} >
+
+    <form>
+    <input type="email" placeholder="email" /><br/>
+    <input type="password" placeholder="password" /><br/>
+    <input type="submit" value="submit" />
+    </form>
+
+    </CoverModal>
+      </div>
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -1,6 +1,33 @@
 import React from 'react'
-import styles from './styles.module.css'
+import styles from './styles.module.css';
+import 'react-screen-modal/dist/index.css';
 
-export const ExampleComponent = ({ text }) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+export const CoverModal = (props) => {
+
+  return (
+    <div
+    id="modal"
+  style={{ backgroundColor:props.color,visibility:props.show==true ? "visible" : "hidden"}}
+  className={styles.maincontainer}>
+  {props.closeButtonRight==true?
+    <p id="close"
+    onClick={()=>props.onClose()}
+    style={{ color:props.closeButtonColor,marginLeft:"95%" }}
+    className={styles.close}>
+    X
+    </p>
+    :
+    <p id="close"
+    onClick={()=>props.onClose()}
+    style={{ color:props.closeButtonColor,marginLeft:"5%" }}
+    className={styles.close}>
+    X
+    </p>
+  }
+   <div
+   style={{ backgroundColor:props.color }}
+   className={styles.container}>
+  {props.children}
+  </div>
+  </div>)
 }
